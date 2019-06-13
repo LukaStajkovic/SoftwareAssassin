8927bbac3b5241bc03485e19d8b131eb0f1f494a
Luka Stajkovic <lukastajcic@gmail.com>
2019-06-13 19:45:13 +0200

    Auto stash before rebase of "refs/heads/master"
    Jebem mu mater bas :)))))

---
+++ Controllers/RedirectController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwareAssassin.Controllers
+{
+    [Route("[controller]")]
+    public class RedirectController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Submit()
+        {
+            return Redirect("https://www.google.com/");
+        }
+    }
+}
